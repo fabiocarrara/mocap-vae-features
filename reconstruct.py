cf1cbc34a95d4969a0da1aa978bd9b348744ee22
Fabio Carrara <fabio.carrara@isti.cnr.it>
2022-07-25 21:46:49 +0200

    clean up

--- reconstruct.py
+++ reconstruct.py
@@ -11,14 +11,6 @@ import body_models
 from datamodules import MoCapDataModule
 from train import LitVAE
 
-BODY_MODELS={
-    'hdm05': (
-        (0, 1), (1, 2), (2, 3), (3, 4), (4, 5), # leg
-        (0, 6), (6, 7), (7, 8), (8, 9), (9, 10), # leg
-        (0, 11), (11, 12), (12, 13), (13, 14), (14, 15), (15, 16), # torso + head
-        (13, 17), (17, 18), (18, 19), (19, 20), (20, 21), (21, 22), (20, 23), # hand
-        (13, 24), (24, 25), (25, 26), (26, 27), (27, 28), (28, 29), (27, 30) # hand
-    ),
 
 def visualize_pose(ax, pose, color, edges):
     artists = []
