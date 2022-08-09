79b84338909ffe10a58f37dae4b416b58737f9bc
Fabio Carrara <fabio.carrara@isti.cnr.it>
2022-08-09 10:34:55 +0200

    added pku-mmd

--- reproduce.sh
+++ reproduce.sh
@@ -5,6 +5,12 @@ set -e
 LATENT_DIMS="256,128,64,32,16,8"
 BETAS="0.1,1,0,0.01,10"
 
+# HDM05 experiments
 python train.py --multirun exp=hdm05/fold1 latent_dim=${LATENT_DIMS} beta=${BETAS}
 python train.py --multirun exp=hdm05/fold2 latent_dim=${LATENT_DIMS} beta=${BETAS}
 python train.py --multirun exp=hdm05/all   latent_dim=${LATENT_DIMS} beta=${BETAS}
+
+# PKU-MMD experiments
+python train.py --multirun exp=pku-mmd/cs  latent_dim=${LATENT_DIMS} beta=${BETAS}
+python train.py --multirun exp=pku-mmd/cv  latent_dim=${LATENT_DIMS} beta=${BETAS}
+python train.py --multirun exp=pku-mmd/all latent_dim=${LATENT_DIMS} beta=${BETAS}
